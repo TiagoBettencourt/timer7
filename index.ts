ec7d18a170361f12d294a44c7092f4071aefaa47
TiagoBettencourt <131544276+TiagoBettencourt@users.noreply.github.com>
2025-03-17 17:06:55 +0000

    Update and rename index.ts to index.js

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { AzureFunction, Context } from "@azure/functions"
-
-const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
-    const timeStamp = new Date().toISOString();
-    context.log('Timer trigger function ran!', timeStamp);
-};
-
-export default timerTrigger;
